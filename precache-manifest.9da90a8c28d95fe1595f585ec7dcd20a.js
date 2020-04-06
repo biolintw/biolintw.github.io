623533b8fc64b45e1af10b293a118ae171ed13f7
Bob Lin <shex.ck@gmail.com>
2020-04-07 01:52:27 +0800

    build site

---
+++ precache-manifest.9da90a8c28d95fe1595f585ec7dcd20a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4412579c1d9549f082ec85a121cb46dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "588f86d6cdff6c669594",
+    "url": "/static/css/2.1e0a2231.chunk.css"
+  },
+  {
+    "revision": "5e7bf1c8323f095aeeb0",
+    "url": "/static/css/main.fe0f3a61.chunk.css"
+  },
+  {
+    "revision": "588f86d6cdff6c669594",
+    "url": "/static/js/2.8e130b62.chunk.js"
+  },
+  {
+    "revision": "5e7bf1c8323f095aeeb0",
+    "url": "/static/js/main.3ffe838d.chunk.js"
+  },
+  {
+    "revision": "c821b15cd6450f2c3f5d",
+    "url": "/static/js/runtime-main.9f893f79.js"
+  }
+]);
